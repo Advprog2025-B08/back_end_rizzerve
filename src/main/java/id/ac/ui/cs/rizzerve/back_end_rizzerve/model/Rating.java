2382ed3a0b79d1d48303e391e3101c13668ede35
DanielAngger <angger.7d@gmail.com>
2025-04-08 22:10:35 +0700

    [GREEN] Add validator for rating class

--- src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/model/Rating.java
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/model/Rating.java
@@ -5,10 +5,19 @@ import lombok.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Builder(builderClassName = "RatingBuilder")
 public class Rating {
     private Long id;
     private User user;
     private Product product;
     private Integer ratingValue;
+
+    public static class RatingBuilder {
+        public Rating build() {
+            if (this.ratingValue == null || this.ratingValue < 1 || this.ratingValue > 5) {
+                throw new IllegalArgumentException("Rating value must be between 1 and 5");
+            }
+            return new Rating(id, user, product, ratingValue);
+        }
+    }
 }
