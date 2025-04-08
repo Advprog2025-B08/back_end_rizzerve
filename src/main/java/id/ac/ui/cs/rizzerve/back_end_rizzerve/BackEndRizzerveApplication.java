8e6e0c84872b6e41fce698e2ab3dbca07527c64e
DanielAngger <angger.7d@gmail.com>
2025-04-08 17:48:26 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/BackEndRizzerveApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.rizzerve.back_end_rizzerve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndRizzerveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndRizzerveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/BackEndRizzerveApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.rizzerve.back_end_rizzerve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndRizzerveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
