701be6b3b0699c7fac901f4247187e715f1bdc47
DanielAngger <angger.7d@gmail.com>
2025-04-08 20:27:34 +0700

    [RED] Add test for rating model

---
+++ src/test/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/model/RatingTest.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.rizzerve.back_end_rizzerve.model;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RatingTest {
+
+    @Test
+    void createRating_Success() {
+        User user = User.builder().id(1L).name("Daniel").build();
+        Product product = Product.builder().id(1L).name("Nasi Goreng").build();
+
+        Rating rating = Rating.builder()
+                .id(1L)
+                .user(user)
+                .product(product)
+                .ratingValue(5)
+                .build();
+
+        assertThat(rating.getUser().getName()).isEqualTo("Daniel");
+        assertThat(rating.getProduct().getName()).isEqualTo("Nasi Goreng");
+        assertThat(rating.getRatingValue()).isEqualTo(5);
+    }
+}
