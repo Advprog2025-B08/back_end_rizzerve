d526ec0e1f53722911f82fa4ab72f7d81f3dde50
nadiarahmadinaa <nadiarahmadinaaristawati@gmail.com>
2025-04-11 18:40:35 +0700

    [GREEN] Implement dependencies and dotenv

---
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_meja/model/Cart.java
@@ -0,0 +1,35 @@
+package id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_meja.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    private String id;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id")
+    private List<Menu> menus;
+
+    public Cart(String id, List<Menu> menus) {
+        this.id = id;
+        this.menus = menus;
+    }
+
+    public List<Menu> getItems() {
+        return this.menus;
+    }
+
+    public void setItems(List<Menu> items) {
+        this.menus = items;
+    }
+}
--- src/test/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_meja/MejaTest.java
+++ src/test/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_meja/MejaTest.java
@@ -1,6 +1,5 @@
-package id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_meja.model;
+package id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_meja;
 
-import id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_meja.model.Meja;
 import id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_meja.model.User;
 import id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_meja.model.Menu;
 import id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_meja.model.Cart;
