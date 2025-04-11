c1c1a9eec4dfb00551f72529bf8d25da9bf8040a
mansizsultan <sultanibnumansiz@gmail.com>
2025-04-11 10:31:30 +0700

    [GREEN] Implement ClearCart command

--- src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_pesanan/command/ClearCartCommand.java
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_pesanan/command/ClearCartCommand.java
@@ -1,10 +1,16 @@
 package id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_pesanan.command;
 
+import id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_pesanan.service.CartService;
+import lombok.AllArgsConstructor;
+
 @AllArgsConstructor
 public class ClearCartCommand implements CartCommand {
+    private final CartService cartService;
+    private final Long userId;
 
     @Override
     public Void execute() {
-
+        cartService.clearCart(userId);
+        return null;
     }
 }
