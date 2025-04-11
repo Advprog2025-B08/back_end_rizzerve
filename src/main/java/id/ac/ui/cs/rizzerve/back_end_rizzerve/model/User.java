dfc50d9eefd6dcb7adb20d40f4db860c44982b3a
mansizsultan <sultanibnumansiz@gmail.com>
2025-04-11 09:58:25 +0700

    Initialize dummy model

--- src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/model/User.java
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/model/User.java
@@ -1,4 +1,12 @@
 package id.ac.ui.cs.rizzerve.back_end_rizzerve.model;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+    private Long id;
+    private String name;
 }
