df2e7f9f17a3804dabae537e15ea6fe2293ee52b
mansizsultan <sultanibnumansiz@gmail.com>
2025-04-11 10:21:53 +0700

    [RED] Add UpdateCart command skeleton

---
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_pesanan/command/UpdateCartCommand.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_pesanan.command;
+
+@AllArgsConstructor
+public class UpdateCartCommand implements CartCommand {
+
+    @Override
+    public CartItem execute() {
+
+    }
+}
