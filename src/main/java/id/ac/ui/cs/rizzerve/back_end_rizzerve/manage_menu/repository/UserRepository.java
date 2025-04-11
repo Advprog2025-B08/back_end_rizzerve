6434bbe60ccbfaece129cc4345bf082048cacdf2
shaneemichael <shanemichaeltanata@gmail.com>
2025-04-11 12:38:16 +0700

    [GREEN] Implement UserRepository class

--- src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_menu/repository/UserRepository.java
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_menu/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_menu.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_menu.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Implement this class
+    Optional<User> findByUsername(String username);
 }
