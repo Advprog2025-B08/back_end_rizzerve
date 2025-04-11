1a91bd4a02455380321f6748f49bf08f4d683ada
mansizsultan <sultanibnumansiz@gmail.com>
2025-04-11 10:12:48 +0700

    [FEAT] Add Command Desgin Pattern

---
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_pesanan/command/CartCommand.java
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_pesanan.command;
+
+import id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_pesanan.model.CartItem;
+
+import java.util.List;
+
+public interface CartCommand {
+    Object execute();
+}
