420285bf091cb1a6a6dd43e5af97aa3b6ea2e027
mansizsultan <sultanibnumansiz@gmail.com>
2025-04-11 10:25:52 +0700

    [RED] Add GetCartItems command skeleton

---
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_pesanan/command/GetCartItemsCommand.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_pesanan.command;
+
+@AllArgsConstructor
+public class GetCartItemsCommand implements CartCommand {
+
+    @Override
+    public List<CartItem> execute() {
+
+    }
+}
