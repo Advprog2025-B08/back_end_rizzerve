cbd4ed0cf12cbfb27042443b22ae1e0fa5be28e7
mansizsultan <sultanibnumansiz@gmail.com>
2025-04-11 10:22:57 +0700

    [GREEN] Implement UpdateCart command

--- src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_pesanan/command/UpdateCartCommand.java
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_pesanan/command/UpdateCartCommand.java
@@ -1,10 +1,18 @@
 package id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_pesanan.command;
 
+import id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_pesanan.model.CartItem;
+import id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_pesanan.service.CartService;
+import lombok.AllArgsConstructor;
+
 @AllArgsConstructor
 public class UpdateCartCommand implements CartCommand {
+    private final CartService cartService;
+    private final Long userId;
+    private final Long menuId;
+    private final int quantityChange;
 
     @Override
     public CartItem execute() {
-
+        return cartService.updateCartItemQuantity(userId, menuId, quantityChange);
     }
 }
