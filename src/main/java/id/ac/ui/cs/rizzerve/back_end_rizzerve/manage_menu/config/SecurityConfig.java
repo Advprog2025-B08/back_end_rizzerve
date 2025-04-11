f164d312ae29b6ca6116d86eb07d2d9580f6cc5e
shaneemichael <shanemichaeltanata@gmail.com>
2025-04-11 14:32:17 +0700

    [RED] Add SecurityConfig skeleton

---
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_menu/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_menu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Implement this method
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // Implement this method
+    }
+}
