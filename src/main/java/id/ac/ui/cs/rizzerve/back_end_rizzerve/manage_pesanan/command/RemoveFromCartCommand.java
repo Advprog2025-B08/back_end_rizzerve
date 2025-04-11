a207b31b972f2f52f8c0726173cddea7c353bd95
mansizsultan <sultanibnumansiz@gmail.com>
2025-04-11 10:18:29 +0700

    [RED] Add RemoveFromCart command skeleton

---
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_pesanan/command/RemoveFromCartCommand.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_pesanan.command;
+
+@AllArgsConstructor
+public class RemoveFromCartCommand implements CartCommand {
+
+    @Override
+    public Void execute() {
+
+    }
+}
