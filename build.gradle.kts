c7361127f93d52b67d647d063ce30e35973cee8d
mansizsultan <sultanibnumansiz@gmail.com>
2025-04-11 08:40:07 +0700

    [REFACTOR] Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,12 +26,16 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.postgresql:postgresql:42.3.4")
+	implementation("me.paulschwarz:spring-dotenv:3.0.0")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("com.h2database:h2")
 }
 
 tasks.withType<Test> {
