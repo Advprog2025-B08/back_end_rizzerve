811031d35cfb8018cb604fbc21786e785796b467
nadiarahmadinaa <nadiarahmadinaaristawati@gmail.com>
2025-04-11 18:57:10 +0700

    [RED] Add Meja repository tests

---
+++ src/test/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_meja/repository/MejaRepositoryTest.java
@@ -0,0 +1,78 @@
+package id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_meja.repository;
+
+import id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_meja.model.Meja;
+import id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_meja.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MejaRepositoryTest {
+
+    private MejaRepository mejaRepository;
+
+    @BeforeEach
+    void setUp() {
+        mejaRepository = new MejaRepository();
+    }
+
+    @Test
+    void testCreateMeja() {
+        Meja meja = new Meja();
+        meja.setNomor(1);
+        mejaRepository.createMeja(meja.getNomor());
+        assertEquals(1, mejaRepository.getAllMejas().size());
+    }
+
+    @Test
+    void testGetMejaByNomor() {
+        mejaRepository.createMeja(10);
+        Meja meja = mejaRepository.getMejaByNomor(10);
+        assertNotNull(meja);
+        assertEquals(10, meja.getNomor());
+    }
+
+    @Test
+    void testUpdateMeja() {
+        mejaRepository.createMeja(5);
+        mejaRepository.updateMeja(5, 7);
+        Meja updatedMeja = mejaRepository.getMejaByNomor(7);
+        assertNotNull(updatedMeja);
+        assertEquals(7, updatedMeja.getNomor());
+    }
+
+    @Test
+    void testDeleteMeja() {
+        mejaRepository.createMeja(4);
+        Meja meja = mejaRepository.getMejaByNomor(4);
+        mejaRepository.deleteMeja(meja.getId());
+        assertNull(mejaRepository.getMejaByNomor(4));
+    }
+
+    @Test
+    void testCheckUniqueTrue() {
+        assertTrue(mejaRepository.checkUnique(100));
+    }
+
+    @Test
+    void testCheckUniqueFalse() {
+        mejaRepository.createMeja(3);
+        assertFalse(mejaRepository.checkUnique(3));
+    }
+
+    @Test
+    void testSetAndDeleteUser() {
+        mejaRepository.createMeja(12);
+        Meja meja = mejaRepository.getMejaByNomor(12);
+
+        User dummyUser = new User(1, "dummy@mail.com", "customer");
+        mejaRepository.setUser(meja.getNomor(), dummyUser.getId());
+
+        assertNotNull(mejaRepository.getMejaByNomor(12).getActiveUser());
+
+        mejaRepository.delUser(12);
+        assertNull(mejaRepository.getMejaByNomor(12).getActiveUser());
+    }
+}
