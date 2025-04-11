02688e4207ecdad9977009669f4092cebeeeb67a
shaneemichael <shanemichaeltanata@gmail.com>
2025-04-11 14:09:32 +0700

    [GREEN] Implement RegisterRequest class

---
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_menu/dto/RegisterRequest.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_menu.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
+public class RegisterRequest {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String role;
+}
