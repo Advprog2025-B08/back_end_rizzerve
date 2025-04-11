69f28cb458c35f088a1af742ddb564593de192a3
daf <daffa199naufal@gmail.com>
2025-04-11 14:53:04 +0700

    [GREEN] implemented temporary Checkout model

--- src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/checkout/model/Checkout.java
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/checkout/model/Checkout.java
@@ -1,4 +1,32 @@
 package id.ac.ui.cs.rizzerve.back_end_rizzerve.checkout.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Checkout {
+
+    private final List<Item> items = new ArrayList<>();
+
+    public void addItem(String name, int quantity, int pricePerItem) {
+        items.add(new Item(name, quantity, pricePerItem));
+    }
+
+    public int getTotalPrice() {
+        return items.stream()
+                .mapToInt(item -> item.quantity * item.pricePerItem)
+                .sum();
+    }
+
+    // Dummy inner class sebagai placeholder sebelum model Menu tersedia
+    static class Item {
+        String name;
+        int quantity;
+        int pricePerItem;
+
+        Item(String name, int quantity, int pricePerItem) {
+            this.name = name;
+            this.quantity = quantity;
+            this.pricePerItem = pricePerItem;
+        }
+    }
 }
