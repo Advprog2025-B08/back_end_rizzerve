49b97e52e80cc190e47db23f8e263a36a1de8505
shaneemichael <shanemichaeltanata@gmail.com>
2025-04-11 14:29:12 +0700

    [GREEN] Implement UserDetailsServiceImpl class

--- src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_menu/service/UserDetailsServiceImpl.java
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/manage_menu/service/UserDetailsServiceImpl.java
@@ -1,12 +1,15 @@
 package id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_menu.service;
 
+import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_menu.model.User;
 import id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_menu.repository.UserRepository;
 
 @Service
@@ -16,11 +19,18 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     public UserDetailsServiceImpl(UserRepository userRepository) {
-        // Implement this constructor
+        this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // Implement this method
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole()))
+        );
     }
 }
