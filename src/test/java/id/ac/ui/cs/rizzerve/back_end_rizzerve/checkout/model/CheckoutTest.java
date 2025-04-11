ea85d472f07f5fcaad836bd4a1d22fad390f4f20
daf <daffa199naufal@gmail.com>
2025-04-11 14:27:48 +0700

    [RED] test for calculate total price of a checkout

--- src/test/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/checkout/model/CheckoutTest.java
+++ src/test/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/checkout/model/CheckoutTest.java
@@ -1,4 +1,15 @@
 package id.ac.ui.cs.rizzerve.back_end_rizzerve.checkout.model;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CheckoutTest {
+    @Test
+    void testCalculateTotalPrice() {
+        Checkout checkout = new Checkout();
+        checkout.addItem("Nasi Goreng", 2, 15000);
+        checkout.addItem("Es Teh", 1, 5000);
+
+        assertEquals(35000, checkout.getTotalPrice());
+    }
 }
