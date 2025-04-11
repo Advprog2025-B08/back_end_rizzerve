06d0c8c303cfd314890f2d08861b9ff93c1b6d8c
shaneemichael <shanemichaeltanata@gmail.com>
2025-04-11 11:27:45 +0700

    [REFACTOR] Add neccessary dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,13 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testRuntimeOnly("com.h2database:h2")
+	implementation("jakarta.validation:jakarta.validation-api")
+    implementation("org.hibernate.validator:hibernate-validator")
+	implementation("org.springframework.data:spring-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("jakarta.persistence:jakarta.persistence-api")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 }
 
 tasks.withType<Test> {
