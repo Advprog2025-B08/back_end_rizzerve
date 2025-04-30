51fd4ba8f379d50d22604dfcc8fdd20702a2f853
DanielAngger <angger.7d@gmail.com>
2025-04-30 10:20:19 +0700

    [GREEN] Add implementation for rating controller

---
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/rating/controller/RatingController.java
@@ -0,0 +1,38 @@
+package id.ac.ui.cs.rizzerve.back_end_rizzerve.rating.controller;
+
+import id.ac.ui.cs.rizzerve.back_end_rizzerve.rating.model.Rating;
+import id.ac.ui.cs.rizzerve.back_end_rizzerve.rating.service.RatingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/ratings")
+@RequiredArgsConstructor
+public class RatingController {
+
+    private final RatingService ratingService;
+
+    @PostMapping
+    public ResponseEntity<Void> createRating(@RequestBody Rating rating) {
+        ratingService.createRating(rating);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Rating> updateRating(@RequestBody Rating rating) {
+        Rating updated = ratingService.updateRating(rating);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteRating(@PathVariable Long id) {
+        ratingService.deleteRating(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/average/{productId}")
+    public Double getAverageRating(@PathVariable("productId") Long productId) {
+        return ratingService.getAverageRatingByProductId(productId);
+    }
+}
