258a1d109cb64ab2dcf0e18a3712a250411e2b84
DanielAngger <angger.7d@gmail.com>
2025-04-30 10:00:10 +0700

    [GREEN] Removing unnecessary test annotation (@SpringBootTest)

--- src/test/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/BackEndRizzerveApplicationTests.java
+++ src/test/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/BackEndRizzerveApplicationTests.java
@@ -3,7 +3,6 @@ package id.ac.ui.cs.rizzerve.back_end_rizzerve;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BackEndRizzerveApplicationTests {
 
 	@Test
