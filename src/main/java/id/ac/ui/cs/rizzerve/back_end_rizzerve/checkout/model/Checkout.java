1cf643bf8461b6c71e62531131cb3fcafb3f8df8
daf <daffa199naufal@gmail.com>
2025-05-02 19:15:36 +0700

    [GREEN] New implementation for Checkout models

--- src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/checkout/model/Checkout.java
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/checkout/model/Checkout.java
@@ -1,21 +1,37 @@
 package id.ac.ui.cs.rizzerve.back_end_rizzerve.checkout.model;
 
-import lombok.Getter;
+import id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_pesanan.model.Cart;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
+@Entity
+@Table(name = "checkout")
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Checkout {
-    private final List<CartItem> items;
-    private final int totalPrice;
-    private final LocalDateTime timestamp;
 
-    public Checkout(List<CartItem> items, int totalPrice, LocalDateTime timestamp) {
-        this.items = items;
-        this.totalPrice = totalPrice;
-        this.timestamp = timestamp;
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(name = "user_id")
+    private Long userId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @Column(name = "total_price")
+    private Integer totalPrice;
+
+    @Column(name = "is_submitted")
+    private Boolean isSubmitted;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 }
-
