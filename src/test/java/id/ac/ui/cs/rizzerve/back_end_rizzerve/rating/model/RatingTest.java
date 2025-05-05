64dafea7f5cae0172638ec6e9acea05b3c147c98
DanielAngger <angger.7d@gmail.com>
2025-05-05 13:49:37 +0700

    [RED] Fixing rating test to refer the real User and Menu (not dummy)

--- src/test/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/rating/model/RatingTest.java
+++ src/test/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/rating/model/RatingTest.java
@@ -1,43 +1,55 @@
 package id.ac.ui.cs.rizzerve.back_end_rizzerve.rating.model;
 
-import id.ac.ui.cs.rizzerve.back_end_rizzerve.rating.model.Product;
-import id.ac.ui.cs.rizzerve.back_end_rizzerve.rating.model.Rating;
-import id.ac.ui.cs.rizzerve.back_end_rizzerve.rating.model.User;
+import id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_menu.model.Menu;
+import id.ac.ui.cs.rizzerve.back_end_rizzerve.manage_menu.model.User;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class RatingTest {
 
     @Test
     void createRating_Success() {
-        User user = User.builder().id(1L).name("Daniel").build();
-        Product product = Product.builder().id(1L).name("Nasi Goreng").build();
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("Daniel");
+        user.setPassword("pass");
+        user.setRole("CUSTOMER");
+
+        Menu menu = new Menu();
+        menu.setId(1L);
+        menu.setName("Nasi Goreng");
 
         Rating rating = Rating.builder()
                 .id(1L)
                 .user(user)
-                .product(product)
+                .menu(menu)
                 .ratingValue(5)
                 .build();
 
-        assertThat(rating.getUser().getName()).isEqualTo("Daniel");
-        assertThat(rating.getProduct().getName()).isEqualTo("Nasi Goreng");
+        assertThat(rating.getUser().getUsername()).isEqualTo("Daniel");
+        assertThat(rating.getMenu().getName()).isEqualTo("Nasi Goreng");
         assertThat(rating.getRatingValue()).isEqualTo(5);
     }
 
     @Test
     void createRating_Failure() {
-        User user = User.builder().id(1L).name("Daniel").build();
-        Product product = Product.builder().id(1L).name("Daniel").build();
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("Daniel");
+        user.setPassword("pass");
+        user.setRole("CUSTOMER");
+
+        Menu menu = new Menu();
+        menu.setId(1L);
+        menu.setName("Nasi Goreng");
 
         assertThrows(IllegalArgumentException.class, () -> {
             Rating.builder()
                     .id(1L)
                     .user(user)
-                    .product(product)
+                    .menu(menu)
                     .ratingValue(6)
                     .build();
         });
