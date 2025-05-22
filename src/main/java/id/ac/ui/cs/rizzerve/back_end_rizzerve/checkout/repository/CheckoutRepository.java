8372049c63a309fb864efc7303d2c1ed4d4014b0
daf <daffa199naufal@gmail.com>
2025-05-22 15:17:22 +0700

    [refactor] refactoring to fix new merges from master

--- src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/checkout/repository/CheckoutRepository.java
+++ src/main/java/id/ac/ui/cs/rizzerve/back_end_rizzerve/checkout/repository/CheckoutRepository.java
@@ -2,6 +2,8 @@ package id.ac.ui.cs.rizzerve.back_end_rizzerve.checkout.repository;
 
 import id.ac.ui.cs.rizzerve.back_end_rizzerve.checkout.model.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
 }
